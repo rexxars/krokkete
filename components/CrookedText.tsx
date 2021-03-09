4ba589d26daf695023dd8c34eaeef6e1d244662b
Espen Hovlandsdal <espen@hovlandsdal.com>
2021-03-08 22:12:50 -0800

    Prefix certain amounts with "så"

--- components/CrookedText.tsx
+++ components/CrookedText.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import {AMOUNT_SLUGS, AMOUNT_SO_PREFIXED} from '../data/constants'
 
 export function getCrookedText({amount, what, reason = ''}) {
+  const index = AMOUNT_SLUGS.indexOf(toAmountSlug(amount))
+  const prefix = AMOUNT_SO_PREFIXED[index] ? `så ` : ''
   const withReason = getReasonText(reason)
-  return `${what || 'Dette'} er ${lcFirst(amount)} krøkkete ${withReason}`.trim()
+  return `${what || 'Dette'} er ${prefix}${lcFirst(amount)} krøkkete ${withReason}`.trim()
 }
 
 export function getReasonText(reason = '') {
@@ -17,4 +20,8 @@ export function CrookedText({amount, what, reason}) {
 
 function lcFirst(str: string): string {
   return (str[0] || '').toLowerCase() + str.slice(1)
+}
+
+function toAmountSlug(amount: string) {
+  return amount.toLowerCase().replace('æ', 'ae')
 }
--- data/constants.ts
+++ data/constants.ts
@@ -1,5 +1,6 @@
 export const ALLOWED_AMOUNTS = ['Litt', 'Veldig', 'Skikkelig', 'Jævla', 'Ekstremt']
 export const AMOUNT_SLUGS = ['litt', 'veldig', 'skikkelig', 'jaevla', 'ekstremt']
+export const AMOUNT_SO_PREFIXED = [false, false, false, true, true]
 export const DEFAULT_AMOUNT = 'veldig'
 export const DOMAIN_NAME = 'krøkkete.com'
 export const IS_LOCAL = process.env.NODE_ENV !== 'production'
